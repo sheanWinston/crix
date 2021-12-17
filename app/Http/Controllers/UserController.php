b46febd32005f6d3ac4b2791a3a83c07379e9be8
winston <sheanwinston@gmail.com>
2021-12-17 13:09:44 +0100

    somtng

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Transaction;
+use App\Mail\CrixMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $title = 'Hurray!!!';
+        $message = 'We are going live in a bit';
+        Mail::to(Auth::user())->send(new CrixMail($title, $message));
+
         $transactions = Auth::user()->transactions()->take(6)->get();
 
         return view('user.dashboard', compact('transactions'));
